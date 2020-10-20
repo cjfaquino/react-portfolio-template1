6d2e255d96569a89f372e03f95e0fc27b0985448
Christian Aquino <cjfaquino@gmail.com>
2020-10-19 23:22:14 -0700

    Updates

---
+++ precache-manifest.b5055a9046b005b3acd18e30dc5e9bf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2115c8483adb69df687b0f8ef95d72d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb5b0b9e6da4617ee270",
+    "url": "/static/css/main.8c52a0a1.chunk.css"
+  },
+  {
+    "revision": "dc0ba26200f899a6a8c7",
+    "url": "/static/js/2.84d176f7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.84d176f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5b0b9e6da4617ee270",
+    "url": "/static/js/main.7177eb6d.chunk.js"
+  },
+  {
+    "revision": "946cc31171ebdb551184",
+    "url": "/static/js/runtime-main.e2163c08.js"
+  }
+]);
