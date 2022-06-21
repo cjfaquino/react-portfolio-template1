6b272a7ecc1a2f9c055e144b1fcce5cd2cd3b29e
Christian Aquino <cjfaquino@gmail.com>
2022-06-21 04:24:46 -0700

    Updates

---
+++ precache-manifest.c351904c85487d00ab4d981d50612adc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac4cd37dbf01214e2027",
+    "url": "/react-second-portfolio/static/css/main.6ed13692.chunk.css"
+  },
+  {
+    "revision": "ac4cd37dbf01214e2027",
+    "url": "/react-second-portfolio/static/js/main.ac4cd37d.chunk.js"
+  },
+  {
+    "revision": "8a596faf5052f652c440",
+    "url": "/react-second-portfolio/static/js/1.8a596faf.chunk.js"
+  },
+  {
+    "revision": "42297e9daa5591c3b421",
+    "url": "/react-second-portfolio/static/js/runtime~main.42297e9d.js"
+  },
+  {
+    "revision": "a5f9846b671c7496baf3ef181c41ed22",
+    "url": "/react-second-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5e576925ce894f664160b39592366151.js"
+  "/react-second-portfolio/precache-manifest.c351904c85487d00ab4d981d50612adc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-second-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
